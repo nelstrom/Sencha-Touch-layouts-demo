e17b14593325a91cb51bf49c63986d75cf8e7ffa
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-18 11:25:46 +0000

    Create 'card' layout.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,14 @@
+// Open up the Web inspector (webkit) or Firebug (Firefox)
+// and try running:
+//
+//      rootPanel.setCard(1)
+//      rootPanel.setCard(2)
+//      rootPanel.setCard(0)
+//
+// The 'card' layout only shows a single panel at a time.
+
+var rootPanel;
+
 Ext.setup({
     onReady: function() {
 
@@ -15,10 +26,17 @@ Ext.setup({
             html: "Amber"
         };
 
-        new Ext.Panel({
+        var green = {
+            dock: 'top',
+            style: "background-color: #3B7E00; color:white;",
+            title: "Green",
+            html: "Green"
+        };
+
+        rootPanel = new Ext.Panel({
             fullscreen: true,
-            layout: 'fit',
-            items: [red, amber],  // fit layout can't accomodate >1 item
+            layout: 'card',
+            items: [red, amber, green],
             dockedItems: []
         });
     }
