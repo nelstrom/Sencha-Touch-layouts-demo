7c888e0770eb61f945d023efc270ed9cc5808992
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-18 11:46:56 +0000

    Create traffic light carousels.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,41 +1,43 @@
-// The 'vbox' and 'hbox' layouts can show multiple panels at once.
-
-var rootPanel;
+// Panels can act as components (i.e. content) or as a container for other 
+// components. So it is possible to mix and match the layout types.
+// Here, a vbox layout contains 3 compenents, each of which is a carousel.
 
 Ext.setup({
     onReady: function() {
 
-        var red = {
-            dock: 'top',
-            style: "background-color: #B22222; color:white;",
-            title: "Red",
-            html: "Red"
-        };
+        var topChanger = new Ext.Carousel({
+            items: [
+                {style: "background-color: #D55B5B;"},
+                {style: "background-color: #B22222;"},
+                {style: "background-color: #7C0505;"}
+            ]
+        });
 
-        var amber = {
-            dock: 'top',
-            style: "background-color: #FFBF00; color:white;",
-            title: "Amber",
-            html: "Amber"
-        };
+        var centerChanger = new Ext.Carousel({
+            items: [
+                {style: "background-color: #FFDD00;"},
+                {style: "background-color: #FFBF00;"},
+                {style: "background-color: #FF8F00;"}
+            ]
+        });
 
-        var green = {
-            dock: 'top',
-            style: "background-color: #3B7E00; color:white;",
-            title: "Green",
-            html: "Green"
-        };
-
-        rootPanel = new Ext.Panel({
+        var bottomChanger = new Ext.Carousel({
+            items: [
+                {style: "background-color: #7BB300;"},
+                {style: "background-color: #3B7E00;"},
+                {style: "background-color: #0E3E00;"}
+            ]
+        });
+        
+        new Ext.Panel({
             fullscreen: true,
             layout: {
-                type: 'hbox',
-                align: 'stretch',
-                pack: 'center'    // try start [default], center or end.
+                type: 'vbox',
+                align: 'stretch'
             },
-            defaults: {flex: 1},  // alternatively, apply flex to each inner item
-            items: [red, amber, green],
-            dockedItems: []
+            defaults: {flex: 1},
+            items: [topChanger, centerChanger, bottomChanger]
         });
+
     }
 });
