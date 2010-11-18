08579609707df18cb1d8fbee91460eb0e7ca4957
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-18 12:04:28 +0000

    Correct indentation for consistency.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,47 +3,47 @@
 // Here, a vbox layout contains 3 compenents, each of which is a carousel.
 
 Ext.setup({
-  onReady: function() {
+    onReady: function() {
 
-    var topChanger = new Ext.Carousel({
-      items: [
-        {cls: "head-1 head"},
-        {cls: "head-2 head"},
-        {cls: "head-3 head"},
-      ]
-    });
+        var topChanger = new Ext.Carousel({
+            items: [
+                {cls: "head-1 head"},
+                {cls: "head-2 head"},
+                {cls: "head-3 head"},
+            ]
+        });
 
-    var centerChanger = new Ext.Carousel({
-      items: [
-        {cls: "torso-1 torso"},
-        {cls: "torso-2 torso"},
-        {cls: "torso-3 torso"},
-      ]
-    });
+        var centerChanger = new Ext.Carousel({
+            items: [
+                {cls: "torso-1 torso"},
+                {cls: "torso-2 torso"},
+                {cls: "torso-3 torso"},
+            ]
+        });
 
-    var bottomChanger = new Ext.Carousel({
-      items: [
-        {cls: "legs-1 legs"},
-        {cls: "legs-2 legs"},
-        {cls: "legs-3 legs"},
-      ]
-    });
+        var bottomChanger = new Ext.Carousel({
+            items: [
+                {cls: "legs-1 legs"},
+                {cls: "legs-2 legs"},
+                {cls: "legs-3 legs"},
+            ]
+        });
 
-    var credits = new Ext.Panel({
-      dock: 'bottom',
-      html: 'Illustrations by <a href="http://www.veryworrying.com/">Laurie Pink</a>'
-    });
+        var credits = new Ext.Panel({
+            dock: 'bottom',
+            html: 'Illustrations by <a href="http://www.veryworrying.com/">Laurie Pink</a>'
+        });
 
-    new Ext.Panel({
-      fullscreen: true,
-      layout: {
-        type: 'vbox',
-        align: 'stretch'
-      },
-      defaults: {flex: 1},
-      items: [topChanger, centerChanger, bottomChanger],
-      dockedItems: [credits]
-    });
+        new Ext.Panel({
+            fullscreen: true,
+            layout: {
+                type: 'vbox',
+                align: 'stretch'
+            },
+            defaults: {flex: 1},
+            items: [topChanger, centerChanger, bottomChanger],
+            dockedItems: [credits]
+        });
 
-  }
+    }
 });
