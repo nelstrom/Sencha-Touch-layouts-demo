c18e2a45e1cd40df98207fc5a33b491c0653049a
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-18 12:22:17 +0000

    Remove stuff that belongs in 02b branch.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,17 +8,10 @@ Ext.setup({
             html: "Red"
         };
 
-        var amber = {
-            dock: 'top',
-            style: "background-color: #FFBF00; color:white;",
-            title: "Amber",
-            html: "Amber"
-        };
-
         new Ext.Panel({
             fullscreen: true,
             layout: 'fit',
-            items: [red, amber],  // fit layout can't accomodate >1 item
+            items: [red],
             dockedItems: []
         });
     }
