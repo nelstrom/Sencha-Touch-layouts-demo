bbd27f501e6f0cde2b3ad552334d4757bf98be23
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-18 11:17:29 +0000

    Create auto layout with red panel.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,18 @@
 Ext.setup({
-    onReady: function() {}
+    onReady: function() {
+
+        var red = {
+          dock: 'top',
+          style: "background-color: #B22222; color:white;",
+          title: "Red",
+          html: "Red"
+        };
+
+        new Ext.Panel({
+            fullscreen: true,
+            layout: 'auto',
+            items: [red],
+            dockedItems: []
+        });
+    }
 });
