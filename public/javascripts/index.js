dbec31980eece32484ce7685e961b0eff00cbc83
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-18 11:57:10 +0000

    Add classes, styles and images for exquisite corpse.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,41 +3,42 @@
 // Here, a vbox layout contains 3 compenents, each of which is a carousel.
 
 Ext.setup({
-    onReady: function() {
+  onReady: function() {
 
-        var topChanger = new Ext.Carousel({
-            items: [
-                {style: "background-color: #D55B5B;"},
-                {style: "background-color: #B22222;"},
-                {style: "background-color: #7C0505;"}
-            ]
-        });
+    var topChanger = new Ext.Carousel({
+      items: [
+        {cls: "head-1 head"},
+        {cls: "head-2 head"},
+        {cls: "head-3 head"},
+      ]
+    });
 
-        var centerChanger = new Ext.Carousel({
-            items: [
-                {style: "background-color: #FFDD00;"},
-                {style: "background-color: #FFBF00;"},
-                {style: "background-color: #FF8F00;"}
-            ]
-        });
+    var centerChanger = new Ext.Carousel({
+      items: [
+        {cls: "torso-1 torso"},
+        {cls: "torso-2 torso"},
+        {cls: "torso-3 torso"},
+      ]
+    });
 
-        var bottomChanger = new Ext.Carousel({
-            items: [
-                {style: "background-color: #7BB300;"},
-                {style: "background-color: #3B7E00;"},
-                {style: "background-color: #0E3E00;"}
-            ]
-        });
-        
-        new Ext.Panel({
-            fullscreen: true,
-            layout: {
-                type: 'vbox',
-                align: 'stretch'
-            },
-            defaults: {flex: 1},
-            items: [topChanger, centerChanger, bottomChanger]
-        });
+    var bottomChanger = new Ext.Carousel({
+      items: [
+        {cls: "legs-1 legs"},
+        {cls: "legs-2 legs"},
+        {cls: "legs-3 legs"},
+      ]
+    });
 
-    }
+    new Ext.Panel({
+      fullscreen: true,
+      layout: {
+        type: 'vbox',
+        align: 'stretch'
+      },
+      defaults: {flex: 1},
+      items: [topChanger, centerChanger, bottomChanger],
+      dockedItems: []
+    });
+
+  }
 });
