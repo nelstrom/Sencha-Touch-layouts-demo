14b6ee51d29fb8d7367591c82e874181795201de
Drew Neil <andrew.jr.neil@gmail.com>
2010-11-18 11:21:35 +0000

    Add a second panel to fit layout.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,16 +2,23 @@ Ext.setup({
     onReady: function() {
 
         var red = {
-          dock: 'top',
-          style: "background-color: #B22222; color:white;",
-          title: "Red",
-          html: "Red"
+            dock: 'top',
+            style: "background-color: #B22222; color:white;",
+            title: "Red",
+            html: "Red"
+        };
+
+        var amber = {
+            dock: 'top',
+            style: "background-color: #FFBF00; color:white;",
+            title: "Amber",
+            html: "Amber"
         };
 
         new Ext.Panel({
             fullscreen: true,
             layout: 'fit',
-            items: [red],
+            items: [red, amber],  // fit layout can't accomodate >1 item
             dockedItems: []
         });
     }
